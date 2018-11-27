b538a23295f97ea53c82d59e0f152fe1705a8032
Tali Scheer <tali.scheer@gmail.com>
2018-11-27 16:30:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,26 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+var price = Math.floor(Math.random() * 101);
+var newObj = new Object({ itemName: item, itemPrice: price });
+ cart.push(newObj);
+ return `${item} has been added to your cart.`;
 }
 
+var myItems = [];
+var myStr = `In your cart, you have`;
+
 function viewCart() {
-  // write your code here
+   if (cart.length === 0) {
+    return `Your shopping cart is empty.`;
+    } else {
+for (let i = 0; i < cart.length; i++) {
+  let myCartStr = `${cart.length === 1 ? '' : cart.length >= 2 ? ' and ' : ''}  ${cart[i].itemName} at $${cart[i].itemPrice}`;
+    cart.insert(-1, ' and ');
+  myItems.push(myCartStr);
+  }
+  }
+  return `${myStr} ${myItems}.`;
 }
 
 function total() {
